61582d9430034de7acc2d45a8515ed70b63b5cc2
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-05-17 01:27:31 +0100

    added async await but gifs showing up in console log need to render it correctly

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const APIKEY = "dfBFNX1oXduOpIZYll5QxOSSmovt8dL6";
-const url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q&limit=6`;
-
 const button = document.querySelector("#search__btn");
+const giphyBox = document.querySelector(".giphy");
 
-async function showGif() {
-  const inputText = document.querySelector("#input__search");
-  const response = await fetch(url);
-  const data = response.json();
+async function showGifs() {
+  const input = document.querySelector("#search__input");
+  const response = await fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${input}&limit=6`
+  );
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+  } else {
+    console.log("Error HTTP:" + response.status);
+  }
 }
 
-button.addEventListener("click", function (e) {
-  e.preventDefault();
-  showGif();
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  showGifs();
 });
 
-window.addEventListener("DOMContentLoaded", () => {
-  showGif(url);
-});
+window.addEventListener("DOMContentLoaded", showGifs);
+
+// const gif = await response.json();
+// const array = gif.data;
+// array.forEach((gif) => {
+//   const giphyBox = `<div class="giphy"><img src= "${data.gif}"></div>`;
+//   document.insertAdjacentElement("beforeend", giphyBox);
+// });
